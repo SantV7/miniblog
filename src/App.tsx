f3fca8649b74d015933461c6285312673870ccdc
Vinícius dos Santos Oliveira <3izuna@gmail.com>
2026-07-16 15:50:50 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './style/global.css'
+function App() {
+
+
+  return (
+    <>
+      <div className=''>
+        <h1>MiniBlog</h1>
+      </div>
+            
+    </>
+  )
+}
+
+export default App
